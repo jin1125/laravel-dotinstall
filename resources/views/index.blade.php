1b1cd0e69e510b50c41295dc04adedfc89fdcecb
jin1125 <shogo.jinta@gmail.com>
2022-01-08 09:33:46 +0900

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<x-layout>
+    <x-slot name='title'>
+        My BBS
+    </x-slot>
+
+    <h1>My BBS</h1>
+    <ul>
+        @forelse ($posts as $post)
+            <li>
+                <a href="{{route('posts.show',$post->id)}}">
+                    {{$post->title}}
+                </a>
+            </li>
+        @empty
+            <li>No posts</li>
+        @endforelse
+    </ul>
+</x-layout>
+
